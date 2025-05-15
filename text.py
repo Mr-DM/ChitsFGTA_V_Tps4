9501e049ac87a73ccc898cad5597caa5d143df45
Mr-Dm <danm306009@gmail.com>
2025-05-15 19:40:50 +0300

    3 commit

---
+++ text.py
@@ -0,0 +1,4 @@
+car_Comet = "Phone - 1-999-266-38 || PlayStation: R1, Круг, R2, Вправо, L1, L2, X, X, Квадрат, R1"
+car_Rapid_GT = "Phone - 1-999-727-4348 || PlayStation: R2, L1, Круг, Вправо, L1, R1, Вправо, Влево, Круг, R2"
+car_Limo = "Phone - 1-999-846-39663 || PlayStation: R2, Вправо, L2, Влево, Влево, R1, L1, Круг, Вправо"
+car_TrashMaster = "Phone - 1-999-872-433 ||Круг, R1, Круг, R1, Влево, Влево, R1, L1, Круг, Вправо"
